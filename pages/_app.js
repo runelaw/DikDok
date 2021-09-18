8fac699615d2647158de3e314b5a757bd5c554e3
Sharad Chand <pepsighan@sharadchand.com>
2021-09-18 13:39:24 +0545

    With a bit of a shade

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
+import { grey } from '@mui/material/colors';
 import theme from 'utils/theme';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GlobalStyles
+        styles={`
+        body {
+          background-color: ${grey[50]};      
+        }`}
+      />
+
       <Component {...pageProps} />
     </ThemeProvider>
   );
