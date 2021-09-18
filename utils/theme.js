9a18cb47c899bf5da22f86134e3ca675ec7241c1
Sharad Chand <pepsighan@sharadchand.com>
2021-09-18 12:34:49 +0545

    Use the lato font

--- utils/theme.js
+++ utils/theme.js
@@ -1,3 +1,29 @@
 import { createTheme } from '@mui/material';
 
-export default createTheme();
+export default createTheme({
+  typography: {
+    fontFamily: '"Lato", "Roboto", "Helvetica", "Arial", sans-serif',
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 700,
+    },
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 700,
+    fontWeightBold: 700,
+  },
+});
