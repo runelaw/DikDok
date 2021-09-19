968222723801732d71e4e172b8605de162713781
Sharad Chand <pepsighan@sharadchand.com>
2021-09-19 11:37:23 +0545

    Use India silhoutte as the bg

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import { Button, Container, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import Navigation from 'components/Navigation';
 import PledgeCard from 'components/PledgeCard';
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
+import india from 'assets/india.png';
 
 export default function Home() {
   return (
@@ -13,36 +16,52 @@ export default function Home() {
         <title>100bhagya - Made a Pledge for #India100</title>
       </Head>
 
-      <Container sx={{ pt: 20, pb: 8 }}>
-        <Typography variant="h3" component="div" textAlign="center">
-          Made a Pledge for{' '}
-          <Typography variant="h3" component="span" color="primary">
-            #India100
-          </Typography>
-        </Typography>
-        <Typography
-          variant="h6"
-          component="div"
-          textAlign="center"
-          color="textSecondary"
-          sx={{ fontWeight: 400, mt: 1 }}
+      <Container sx={{ pb: 8, position: 'relative' }}>
+        <Box
+          sx={{
+            '& img': {
+              '-webkit-filter':
+                'contrast(0) sepia(0) hue-rotate(180deg) saturate(0%) brightness(170%)',
+              filter:
+                'contrast(0) sepia(0) hue-rotate(180deg) saturate(0%) brightness(170%)',
+              opacity: 1,
+            },
+          }}
         >
-          25 years is long enough to create a meaningful impact, but not long
-          enough to wait and watch.
-        </Typography>
+          <Image src={india} alt="India" />
+        </Box>
 
-        <Stack alignItems="center" sx={{ mt: 8 }}>
-          <Link href="/make-a-pledge" passHref>
-            <Button component="a" variant="contained" size="large">
-              Make a Pledge Today
-            </Button>
-          </Link>
-          <Typography color="textSecondary" sx={{ mt: 1 }}>
-            “The future depends on what you do today.” - Gandhi
+        <Box sx={{ position: 'absolute', top: 24 * 8, left: 0, width: '100%' }}>
+          <Typography variant="h3" component="div" textAlign="center">
+            Made a Pledge for{' '}
+            <Typography variant="h3" component="span" color="primary">
+              #India100
+            </Typography>
           </Typography>
-        </Stack>
+          <Typography
+            variant="h6"
+            component="div"
+            textAlign="center"
+            color="textSecondary"
+            sx={{ fontWeight: 400, mt: 1 }}
+          >
+            25 years is long enough to create a meaningful impact, but not long
+            enough to wait and watch.
+          </Typography>
 
-        <Typography variant="h5" component="div" sx={{ mt: 20, mb: 3 }}>
+          <Stack alignItems="center" sx={{ mt: 8 }}>
+            <Link href="/make-a-pledge" passHref>
+              <Button component="a" variant="contained" size="large">
+                Make a Pledge Today
+              </Button>
+            </Link>
+            <Typography color="textSecondary" sx={{ mt: 1 }}>
+              “The future depends on what you do today.” - Gandhi
+            </Typography>
+          </Stack>
+        </Box>
+
+        <Typography variant="h5" component="div" sx={{ mt: 8, mb: 3 }}>
           What pledges did people make?
         </Typography>
         <PledgeCard isDescriptionShown />
