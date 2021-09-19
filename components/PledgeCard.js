6ac7e37d2eac8d60a0dd5f3ed67d898ffba75470
Sharad Chand <pepsighan@sharadchand.com>
2021-09-19 11:45:10 +0545

    Make the card a bit tinier

--- components/PledgeCard.js
+++ components/PledgeCard.js
@@ -4,11 +4,13 @@ import {
   CardActions,
   CardContent,
   Chip,
+  IconButton,
   Stack,
   Typography,
 } from '@mui/material';
 import { Box } from '@mui/system';
 import { MdArrowUpward } from 'react-icons/md';
+import { MdShare, MdOpenInNew } from 'react-icons/md';
 
 export default function PledgeCard({ isDescriptionShown }) {
   return (
@@ -16,10 +18,11 @@ export default function PledgeCard({ isDescriptionShown }) {
       <Button
         sx={{
           flexDirection: 'column',
-          width: 200,
+          width: 150,
           borderRadius: 0,
           borderRight: '1px solid',
           borderColor: 'grey.300',
+          flexShrink: 0,
         }}
       >
         <MdArrowUpward size={20} />
@@ -28,35 +31,33 @@ export default function PledgeCard({ isDescriptionShown }) {
         </Box>
       </Button>
 
-      <Box>
-        <CardContent sx={{ py: 1 }}>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            John Doe
+      <CardContent sx={{ pt: 1, pb: 1.5 }}>
+        <Typography variant="h6" sx={{ fontSize: '1.1rem' }}>
+          Irure aute culpa id velit irure duis occaecat eiusmod consectetur.
+        </Typography>
+
+        {isDescriptionShown && (
+          <Typography>
+            Ipsum minim amet voluptate laborum. Eiusmod esse occaecat ut
+            cupidatat quis in exercitation Lorem consectetur mollit id officia
+            voluptate Lorem.
           </Typography>
+        )}
 
-          <Typography variant="h6" sx={{ fontSize: '1.1rem' }}>
-            Irure aute culpa id velit irure duis occaecat eiusmod consectetur
-            aliquip esse.
-          </Typography>
+        <Stack direction="row" sx={{ mt: 1 }}>
+          <Chip label="Education" size="small" sx={{ mr: 1 }} />
+          <Chip label="Finance" size="small" />
+        </Stack>
+      </CardContent>
 
-          {isDescriptionShown && (
-            <Typography>
-              Ipsum minim amet voluptate laborum. Eiusmod esse occaecat ut
-              cupidatat quis in exercitation Lorem consectetur mollit id officia
-              voluptate Lorem.
-            </Typography>
-          )}
-
-          <Stack direction="row" sx={{ mt: 1 }}>
-            <Chip label="Education" size="small" sx={{ mr: 1 }} />
-            <Chip label="Finance" size="small" />
-          </Stack>
-        </CardContent>
-        <CardActions sx={{ justifyContent: 'space-between', px: 2, pt: 0 }}>
-          <Button size="small">Open Link</Button>
-          <Button size="small">Share</Button>
-        </CardActions>
-      </Box>
+      <CardActions sx={{ alignItems: 'flex-start', px: 2, pt: 1 }}>
+        <IconButton size="small">
+          <MdShare />
+        </IconButton>
+        <IconButton size="small">
+          <MdOpenInNew />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
