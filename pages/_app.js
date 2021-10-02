8da618fda11aa9f27cec708d4a84a463f2bcb3d6
Sharad Chand <pepsighan@sharadchand.com>
2021-10-02 17:19:06 +0545

    Initialize the auth state on load

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
 import { grey } from '@mui/material/colors';
+import { useInitializeAuthStore } from 'store/auth';
 import theme from 'utils/theme';
 
 export default function MyApp({ Component, pageProps }) {
+  useInitializeAuthStore();
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
