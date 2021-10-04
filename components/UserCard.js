12a78c4a9db2b4354f7c80d9568487a6ada97303
Sharad Chand <pepsighan@sharadchand.com>
2021-10-04 08:46:40 +0545

    Show need to sign in message

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,20 +1,43 @@
-import { Avatar, Paper, Typography } from '@mui/material';
+import { Avatar, Button, Paper, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { useCallback } from 'react';
-import { useAuth } from 'store/auth';
+import { loggedIn, useAuth, useLogin } from 'store/auth';
 
 export default function UserCard() {
   const user = useAuth(useCallback((state) => state.user, []));
+  const isLoggedIn = useAuth(useCallback((state) => state.isLoggedIn, []));
+  const login = useLogin();
 
-  return user ? (
-    <Paper sx={{ display: 'flex', alignItems: 'center', px: 2, py: 1 }}>
-      <Avatar src={user.photoURL} alt="User Profile" />
-      <Box sx={{ ml: 3 }}>
-        <Typography variant="subtitle2">{user.name}</Typography>
-        <Typography variant="body2" color="textSecondary">
-          {user.email}
-        </Typography>
-      </Box>
-    </Paper>
-  ) : null;
+  return (
+    <>
+      {user && (
+        <Paper sx={{ display: 'flex', alignItems: 'center', px: 2, py: 1 }}>
+          <Avatar src={user.photoURL} alt="User Profile" />
+          <Box sx={{ ml: 3 }}>
+            <Typography variant="subtitle2">{user.name}</Typography>
+            <Typography variant="body2" color="textSecondary">
+              {user.email}
+            </Typography>
+          </Box>
+        </Paper>
+      )}
+
+      {isLoggedIn === loggedIn.false && (
+        <Paper
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            px: 2,
+            py: 2,
+          }}
+        >
+          <Typography>You need to be signed in to make a pledge.</Typography>
+          <Button variant="contained" onClick={login}>
+            Sign in with Google
+          </Button>
+        </Paper>
+      )}
+    </>
+  );
 }
