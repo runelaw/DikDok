c20b7ffa637bdc92de1e068a84ce6103237d5bb0
Sharad Chand <pepsighan@sharadchand.com>
2021-10-04 09:31:39 +0545

    Validate the make a pledge form

--- pages/make-a-pledge.js
+++ pages/make-a-pledge.js
@@ -1,3 +1,4 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
   Button,
   Container,
@@ -13,16 +14,44 @@ import { useCallback } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { loggedIn, useAuth } from 'store/auth';
 import materialRegister from 'utils/materialRegister';
+import { z } from 'zod';
+
+const urlRegex =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+const schema = z.object({
+  title: z
+    .string()
+    .min(1, { message: 'Cannot be empty' })
+    .max(80, { message: 'Cannot be more than 80 characters' }),
+  link: z
+    .string()
+    .optional()
+    .refine((val) => val === '' || urlRegex.test(val), {
+      message: 'Not a valid link',
+    }),
+  description: z
+    .string()
+    .min(1, { message: 'Cannot be empty' })
+    .max(140, { message: 'Cannot be more than 140 characters' }),
+  tag: z.string().min(1, { message: 'Cannot be empty' }),
+});
 
 export default function MakeAPledge() {
   const isLoggedIn = useAuth(useCallback((state) => state.isLoggedIn, []));
-  const { register, handleSubmit, control } = useForm({
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       title: '',
       link: '',
       description: '',
       tag: '',
     },
+    resolver: zodResolver(schema),
   });
 
   const onSubmit = useCallback((state) => {
@@ -60,12 +89,17 @@ export default function MakeAPledge() {
               label="Title"
               size="small"
               sx={{ bgcolor: 'white', mt: 4 }}
+              error={!!errors.title}
+              helperText={errors.title?.message}
+              required
             />
             <TextField
               {...materialRegister(register, 'link')}
               label="Link"
               size="small"
               sx={{ bgcolor: 'white', mt: 2 }}
+              error={!!errors.link}
+              helperText={errors.link?.message}
             />
             <TextField
               {...materialRegister(register, 'description')}
@@ -74,17 +108,23 @@ export default function MakeAPledge() {
               minRows={8}
               size="small"
               sx={{ bgcolor: 'white', mt: 2 }}
+              error={!!errors.description}
+              helperText={errors.description?.message}
+              required
             />
             <Controller
               name="tag"
               control={control}
-              render={({ field }) => (
+              render={({ field, fieldState }) => (
                 <TextField
                   {...field}
                   label="Tag"
                   size="small"
                   sx={{ bgcolor: 'white', mt: 2 }}
                   select
+                  error={!!fieldState.error}
+                  helperText={fieldState.error?.message}
+                  required
                 >
                   <MenuItem value="education">Education</MenuItem>
                   <MenuItem value="agriculture">Agriculture</MenuItem>
