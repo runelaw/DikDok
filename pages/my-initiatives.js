47d4a38b82770396c954efb97c38b49504fb6577
Sharad Chand <pepsighan@sharadchand.com>
2021-10-05 12:54:24 +0545

    Redirect if not logged in

--- pages/my-initiatives.js
+++ pages/my-initiatives.js
@@ -1,16 +1,35 @@
 import { Container, Typography } from '@mui/material';
-import HomeMainSection from 'components/HomeMainSection';
 import Navigation from 'components/Navigation';
+import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
+import { useCallback, useEffect } from 'react';
+import { loggedIn, useAuth } from 'store/auth';
 
 export default function MyInitiatives() {
+  const isLoggedIn = useAuth(useCallback((state) => state.isLoggedIn, []));
+
+  const { push } = useRouter();
+  useEffect(() => {
+    if (isLoggedIn === loggedIn.false) {
+      push('/');
+    }
+  }, [isLoggedIn, push]);
+
+  const seo = (
+    <Head>
+      <title>100bhagya - My Initiatives</title>
+    </Head>
+  );
+
+  if (isLoggedIn !== loggedIn.true) {
+    return seo;
+  }
+
   return (
     <>
-      <Navigation />
+      {seo}
 
-      <Head>
-        <title>100bhagya - My Initiatives</title>
-      </Head>
+      <Navigation />
 
       <Container sx={{ pt: 8, pb: 8 }}>
         <Typography variant="h5" textAlign="center">
