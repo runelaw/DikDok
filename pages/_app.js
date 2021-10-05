cf4ca8bca79e32d7b76bf81d8c6c0d74cb9d000e
Sharad Chand <pepsighan@sharadchand.com>
2021-10-05 13:07:56 +0545

    Change the cache if user logs out

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,30 @@
 import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
 import { grey } from '@mui/material/colors';
+import { useCallback, useMemo } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { useInitializeAuthStore } from 'store/auth';
+import { useAuth, useInitializeAuthStore } from 'store/auth';
 import { useInitializeFirebase } from 'store/firebase';
 import theme from 'utils/theme';
-
-const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
   useInitializeFirebase();
   useInitializeAuthStore();
+
+  const uid = useAuth(useCallback((state) => state.user?.uid, []));
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // Do not automatically keep on refetching the same data.
+            staleTime: Infinity,
+          },
+        },
+      }),
+    // When the user changes, reset the query cache in the client.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [uid]
+  );
 
   return (
     <ThemeProvider theme={theme}>
