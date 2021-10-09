078239e6b83c8bef801e0960c34f7185a79a7145
Sharad Chand <pepsighan@sharadchand.com>
2021-10-09 15:27:59 +0545

    Connect with the form state

--- components/PromoteForm.js
+++ components/PromoteForm.js
@@ -1,11 +1,14 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Stack, TextField, Typography } from '@mui/material';
 import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
 import { loggedIn, useAuth } from 'store/auth';
+import materialRegister from 'utils/materialRegister';
+import { z } from 'zod';
 import SignInWithGoogle from './SignInWithGoogle';
 
 export default function PromoteForm() {
   const isLoggedIn = useAuth(useCallback((state) => state.isLoggedIn, []));
-  const user = useAuth(useCallback((state) => state.user, []));
 
   if (isLoggedIn === loggedIn.loading) {
     // Show a loading spinner.
@@ -27,8 +30,33 @@ export default function PromoteForm() {
     );
   }
 
+  return <Form />;
+}
+
+const schema = z.object({
+  name: z.string().min(1, { message: 'Cannot be empty' }),
+});
+
+function Form() {
+  const user = useAuth(useCallback((state) => state.user, []));
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: user.name || '',
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = useCallback((state) => {
+    console.log(state);
+  }, []);
+
   return (
-    <Stack spacing={2} sx={{ mt: 4 }}>
+    <Stack
+      component="form"
+      spacing={2}
+      sx={{ mt: 4 }}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <TextField
         label="Email"
         InputProps={{
@@ -36,10 +64,16 @@ export default function PromoteForm() {
           disabled: true,
         }}
         size="small"
-        value={user?.email}
+        value={user.email}
       />
-      <TextField label="Name" size="small" />
-      <Button variant="contained">Pledge Now</Button>
+      <TextField
+        label="Name"
+        size="small"
+        {...materialRegister(register, 'name')}
+      />
+      <Button type="submit" variant="contained">
+        Pledge Now
+      </Button>
     </Stack>
   );
 }
