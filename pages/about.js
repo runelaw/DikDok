d51237ba84a1b555c478826864638df7e71466b5
Sharad Chand <pepsighan@sharadchand.com>
2021-10-17 14:16:57 +0545

    Update the about us page with new content

--- pages/about.js
+++ pages/about.js
@@ -46,7 +46,37 @@ export default function About() {
             color: 'white',
           }}
         >
-          <Typography variant="h4">Launching Saubhagya, India!</Typography>
+          <Typography variant="h4">
+            Saubhagya, India 100 / Preamble and Vision
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            sx={{
+              fontWeight: 400,
+              mt: 2,
+            }}
+          >
+            India celebrated 75th Independence Day on Aug 15, 2021. In 25 years,
+            we celebrate India at 100! The next 25 years of our active lives
+            coincide with the 25 years to India100. How will we contribute?
+          </Typography>
+
+          <Typography
+            variant="h5"
+            component="p"
+            sx={{
+              fontWeight: 400,
+              mt: 4,
+            }}
+          >
+            {'"'}25 years is long enough to create a meaningful impact, but not
+            long enough to wait and watch.{'"'}
+          </Typography>
+
+          <Typography variant="h4" sx={{ mt: 4 }}>
+            Launching Saubhagya, India!
+          </Typography>
           <Typography
             variant="h6"
             component="p"
@@ -64,22 +94,38 @@ export default function About() {
           <Typography
             variant="h6"
             component="p"
-            sx={{ fontWeight: 400, mt: 2 }}
+            sx={{
+              fontWeight: 400,
+              mt: 1,
+              ml: 2,
+            }}
           >
-            ● The first milestone is to have 1,000,000 ideas connected to the
-            movement <br />
-            ● Support idea development and collaboration across key themes for
-            India 100 <br />
-            ● A showcase of ideas with unrelenting potential and impact created
-            by individuals/teams <br />
-            ● At 140 Crores, we are 1/6th of this world
+            ● Platform to share and engage with ideas to support idea
+            development and collaboration across key themes for India 100.{' '}
+            <br />● A showcase of ideas with unrelenting potential and impact
+            created by individuals/teams.
+          </Typography>
+
+          <Typography variant="h4" sx={{ mt: 4 }}>
+            Why Saubhagya! Why Now!
+          </Typography>
+          <Typography
+            variant="h6"
+            component="p"
+            sx={{
+              fontWeight: 400,
+              mt: 1,
+              ml: 2,
+            }}
+          >
+            ● At 140 Crores, we are 1/6th of this world.
             <br />
             ● With 60% of the population between 15-54 years, we have the
-            largest number of active minds and energies <br />● The next 25
-            years of our active lives coincide with the 25 years to India100.
-            How will we contribute? This is our responsibility and
-            Saubhagya(fortune) to participate and contribute to the Saubhagya,
-            India100 movement
+            largest number of active minds and energies.
+            <br />● The next 25 years of our active lives coincide with the 25
+            years to India100. How will we contribute? This is our
+            responsibility and Saubhagya(fortune) to participate and contribute
+            to the Saubhagya, India100 movement.
           </Typography>
         </Container>
       </Box>
