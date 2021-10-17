8bbdcdadcffdc23c6d4e40017f07f24741fd946e
Sharad Chand <pepsighan@sharadchand.com>
2021-10-17 18:05:43 +0545

    A separate make a pledge page

---
+++ pages/make-a-pledge.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Navigation from 'components/Navigation';
+
+export default function MakeAPledge() {
+  return (
+    <>
+      <Head>
+        <title>100bhagya - Make a Pledge</title>
+      </Head>
+
+      <Navigation />
+    </>
+  );
+}
