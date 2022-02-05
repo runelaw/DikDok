c394638de8118604616bd8a14547d19d4bafaacf
Sharad Chand <pepsighan@sharadchand.com>
2022-02-05 14:28:03 +0545

    Cleanup the main section

--- components/FlipCounter.js
+++ components/FlipCounter.js
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack } from '@mui/material';
 import { Box } from '@mui/system';
 import '@pqina/flip/dist/flip.min.css';
 import { DateTime } from 'luxon';
@@ -8,22 +8,17 @@ export default function FlipCounter() {
   const yearRef = useRef();
   const monthRef = useRef();
   const dayRef = useRef();
-  const hourRef = useRef();
-  const minuteRef = useRef();
-  const secondRef = useRef();
 
   useEffect(() => {
     const sauSaal = DateTime.local(2046, 8, 15, 0, 0, 0, 0);
     let interval;
 
     import('@pqina/flip').then(({ default: Tick }) => {
-      const { years, months, days, hours, minutes, seconds } = sauSaal.diffNow([
+      const { years, months, days } = sauSaal.diffNow([
         'years',
         'months',
         'days',
         'hours',
-        'minutes',
-        'seconds',
       ]);
       const yearTicker = Tick.DOM.create(yearRef.current, {
         value: `${years}Y `,
@@ -34,68 +29,44 @@ export default function FlipCounter() {
       const dayTicker = Tick.DOM.create(dayRef.current, {
         value: `${days}D`,
       });
-      const hourTicker = Tick.DOM.create(hourRef.current, {
-        value: doubleDigit(hours) + ':',
-      });
-      const minuteTicker = Tick.DOM.create(minuteRef.current, {
-        value: doubleDigit(minutes),
-      });
 
       interval = setInterval(() => {
-        const { years, months, days, hours, minutes } = sauSaal.diffNow([
+        const { years, months, days } = sauSaal.diffNow([
           'years',
           'months',
           'days',
           'hours',
-          'minutes',
-          'seconds',
         ]);
 
         yearTicker.value = `${years}Y `;
         monthTicker.value = `${months}M `;
         dayTicker.value = `${days}D`;
-        hourTicker.value = doubleDigit(hours) + ':';
-        minuteTicker.value = doubleDigit(minutes);
       }, 1000);
     });
 
     const year = yearRef.current;
     const month = monthRef.current;
     const day = dayRef.current;
-    const hour = hourRef.current;
-    const minute = minuteRef.current;
     return () => {
       import('@pqina/flip').then(({ default: Tick }) => {
         Tick.DOM.destroy(year);
         Tick.DOM.destroy(month);
         Tick.DOM.destroy(day);
-        Tick.DOM.destroy(hour);
-        Tick.DOM.destroy(minute);
       });
       clearInterval(interval);
     };
   }, []);
 
   return (
-    <>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        sx={{ fontSize: { xs: '1.5rem', sm: '2rem' } }}
-      >
-        <Counter ref={yearRef} name="Year" />
-        <Counter ref={monthRef} name="Month" />
-        <Counter ref={dayRef} name="Day" />
-      </Stack>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        sx={{ fontSize: { xs: '2rem', sm: '2rem' }, mt: 2 }}
-      >
-        <Counter ref={hourRef} name="Hour" />
-        <Counter ref={minuteRef} name="Minute" />
-      </Stack>
-    </>
+    <Stack
+      direction="row"
+      justifyContent="center"
+      sx={{ fontSize: { xs: '1.5rem', sm: '2rem' } }}
+    >
+      <Counter ref={yearRef} name="Year" />
+      <Counter ref={monthRef} name="Month" />
+      <Counter ref={dayRef} name="Day" />
+    </Stack>
   );
 }
 
@@ -122,11 +93,3 @@ const Counter = forwardRef(function Counter({ name }, ref) {
     </Box>
   );
 });
-
-function doubleDigit(num) {
-  const text = `0${num}`;
-  if (text.length === 2) {
-    return text;
-  }
-  return text.substring(1);
-}
--- components/HomeMainSection.js
+++ components/HomeMainSection.js
@@ -8,97 +8,37 @@ import Link from 'next/link';
 
 export default function HomeMainSection() {
   return (
-    <Box sx={{ position: 'relative', overflow: 'hidden' }}>
-      <Box
-        sx={{
-          position: 'absolute',
-          top: { xs: '30%', sm: '50%' },
-          left: '50%',
-          width: '90%',
-          height: '90%',
-          zIndex: -100,
-          transform: 'translate(-50%, -50%)',
-          '& img': {
-            WebkitFilter:
-              'contrast(0) sepia(0) hue-rotate(180deg) saturate(0%) brightness(170%)',
-            filter:
-              'contrast(0) sepia(0) hue-rotate(180deg) saturate(0%) brightness(170%)',
-            opacity: 1,
-          },
-        }}
+    <Container sx={{ pt: 40, pb: 24 }}>
+      <Typography
+        variant="h1"
+        component="div"
+        textAlign="center"
+        sx={{ mt: -2 }}
       >
-        <Box sx={{ position: 'relative', width: '100%', height: '100%' }}>
-          <Image
-            src={india}
-            alt="India Landscape"
-            layout="fill"
-            objectFit="contain"
-          />
-        </Box>
+        Make a Pledge <br />
+        for{' '}
+        <Typography variant="h1" component="span" color="primary">
+          #India100
+        </Typography>
+      </Typography>
+      <Typography
+        variant="h6"
+        component="div"
+        textAlign="center"
+        color="textSecondary"
+        sx={{ fontWeight: 400, mt: 1 }}
+      >
+        25 years is long enough to create a <br />
+        meaningful impact, but not long <br />
+        enough to wait and watch.
+      </Typography>
+
+      <Box sx={{ mt: 4 }}>
+        <FlipCounter />
+        <Typography color="textSecondary" sx={{ mt: 1, textAlign: 'center' }}>
+          Time is running out!
+        </Typography>
       </Box>
-
-      <Container sx={{ pt: 4, pb: 12 }}>
-        <Stack
-          sx={{
-            alignItems: 'center',
-            mt: {
-              xs: 8,
-              md: 20,
-            },
-          }}
-        >
-          <Box
-            sx={{
-              maxWidth: hundred.width / 7,
-              maxHeight: hundred.height / 7,
-              width: '100%',
-              height: '100%',
-            }}
-          >
-            <Image src={hundred} alt="Hundred" layout="responsive" />
-          </Box>
-        </Stack>
-
-        <Typography
-          variant="h3"
-          component="div"
-          textAlign="center"
-          sx={{ mt: -2 }}
-        >
-          Make a Pledge for{' '}
-          <Typography variant="h3" component="span" color="primary">
-            #India100
-          </Typography>
-        </Typography>
-        <Typography
-          variant="h6"
-          component="div"
-          textAlign="center"
-          color="textSecondary"
-          sx={{ fontWeight: 400, mt: 1 }}
-        >
-          25 years is long enough to create a meaningful impact, but not long
-          enough to wait and watch.
-        </Typography>
-
-        <Stack alignItems="center" sx={{ mt: 8 }}>
-          <Link href="/make-a-pledge" passHref>
-            <Button component="a" variant="contained" size="large">
-              Make a Pledge Today
-            </Button>
-          </Link>
-          <Typography color="textSecondary" sx={{ mt: 1, textAlign: 'center' }}>
-            “The future depends on what you do today.” - Gandhi
-          </Typography>
-        </Stack>
-
-        <Box sx={{ mt: 4 }}>
-          <FlipCounter />
-          <Typography color="textSecondary" sx={{ mt: 1, textAlign: 'center' }}>
-            Time is running out.
-          </Typography>
-        </Box>
-      </Container>
-    </Box>
+    </Container>
   );
 }
