93261dc67a14bcaca0b461138548a59354b0b0e5
Sharad Chand <pepsighan@sharadchand.com>
2022-02-05 15:18:56 +0545

    Re-add the pledge store

---
+++ store/pledge.js
@@ -0,0 +1,67 @@
+import { useCallback } from 'react';
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  increment,
+  query,
+  serverTimestamp,
+  setDoc,
+  where,
+} from 'firebase/firestore';
+import { firestore } from 'store/firebase';
+import { useQuery, useQueryClient } from 'react-query';
+
+/**
+ * Gets the pledges count.
+ */
+export function usePledgesCount() {
+  return useQuery('get-pledges-count', () =>
+    getDoc(doc(firestore, 'pledgesCount', 'count')).then(
+      (result) => result.get('count') ?? 0
+    )
+  );
+}
+
+/**
+ * Hook to make a pledge.
+ */
+export function useMakePledge() {
+  const queryClient = useQueryClient();
+
+  return useCallback(
+    async ({ name, email }) => {
+      const pledge = await getDocs(
+        query(collection(firestore, 'pledges'), where('email', '==', email))
+      );
+      if (!pledge.empty) {
+        return;
+      }
+
+      await setDoc(
+        doc(collection(firestore, 'pledges')),
+        {
+          name,
+          email,
+          createdAt: serverTimestamp(),
+        },
+        {
+          mergeFields: ['name', 'email'],
+        }
+      );
+
+      // Increment the counter.
+      await setDoc(
+        doc(firestore, 'pledgesCount', 'count'),
+        {
+          count: increment(1),
+        },
+        { merge: true }
+      );
+
+      await queryClient.invalidateQueries('get-pledges-count');
+    },
+    [queryClient]
+  );
+}
