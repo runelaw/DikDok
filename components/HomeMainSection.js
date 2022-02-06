dcb54174c5677e8e74dee0f14e907c1c4e0d24b1
Sharad Chand <pepsighan@sharadchand.com>
2022-02-06 08:58:16 +0545

    Put the bg on the home page

--- components/HomeMainSection.js
+++ components/HomeMainSection.js
@@ -3,18 +3,61 @@ import { Box } from '@mui/system';
 import TimeCounter from 'components/TimeCounter';
 import { usePledgesCount } from 'store/pledge';
 import { LoadingButton } from '@mui/lab';
+import Image from 'next/image';
+import skipping from 'assets/skipping.png';
 
 export default function HomeMainSection() {
   const { data: count } = usePledgesCount();
 
   return (
-    <Container sx={{ pt: 20, pb: { md: 12 } }}>
+    <Container sx={{ position: 'relative', pt: 20, pb: { md: 12 } }}>
+      <Box
+        sx={{
+          borderRadius: '100%',
+          overflow: 'hidden',
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          zIndex: -1,
+          transform: 'translate(-50%, -20%)',
+          display: {
+            xs: 'none',
+            md: 'block',
+          },
+        }}
+      >
+        <Image src={skipping} />
+      </Box>
+
+      <Box
+        sx={{
+          overflow: 'hidden',
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          zIndex: -1,
+          width: '100%',
+          height: '100%',
+          display: {
+            xs: 'block',
+            md: 'none',
+          },
+        }}
+      >
+        <Image src={skipping} layout="fixed" />
+      </Box>
+
       <Typography
         variant="h1"
         component="div"
         textAlign="center"
         sx={{
           mt: -2,
+          textShadow:
+            '-1px 0px 0 white,' +
+            ' 0px -1px 0 white,' +
+            ' 0px 1px 0 white,' +
+            ' 1px 0px 0 white',
           fontSize: {
             xs: '2.5rem',
             sm: '3rem',
@@ -43,8 +86,15 @@ export default function HomeMainSection() {
         variant="h6"
         component="div"
         textAlign="center"
-        color="textSecondary"
-        sx={{ fontWeight: 400, mt: 1 }}
+        sx={{
+          fontWeight: 'medium',
+          mt: 1,
+          textShadow:
+            '-1px 0px 0 white,' +
+            ' 0px -1px 0 white,' +
+            ' 0px 1px 0 white,' +
+            ' 1px 0px 0 white',
+        }}
       >
         25 years is long enough to create a <br />
         meaningful impact, but not long <br />
@@ -64,7 +114,7 @@ export default function HomeMainSection() {
         </Typography>
       </Box>
 
-      <Stack alignItems="center" sx={{ mt: 4 }}>
+      <Stack alignItems="center" sx={{ mt: 4, pb: 2 }}>
         <Typography
           variant="h6"
           color="primary"
