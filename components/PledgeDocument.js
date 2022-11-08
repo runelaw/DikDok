b1431cf4b5d6844ec6e1b45c90fea05d01d3850d
Prakhar Gusain <prakhargusain@gmail.com>
2022-11-08 15:36:10 +0530

    setup: added legacy support in script for nodejs>16.6

--- components/PledgeDocument.js
+++ components/PledgeDocument.js
@@ -78,7 +78,7 @@ export default function PledgeDocument({ but }) {
         }`}
         onClick={onOpen}
       >
-        <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-green-600 transition-colors duration-300 ease-out border-2 border-green-600 rounded-lg group-hover:text-white">
+        <span className="relative z-10 block px-5 py-3 overflow-hidden leading-tight text-green-600 font-semibold transition-colors duration-300 ease-out border-2 border-green-600 rounded-lg group-hover:text-white">
           <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
           <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-green-600 group-hover:-rotate-180 ease"></span>
           <span className="relative">Pledge</span>
